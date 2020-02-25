2f77c0bd0dde936c708b86447b3a7ee4506be045
sgowda91 <sunil.gowda@kellogg.northwestern.edu>
2020-02-25 06:43:16 +0000

    update

--- app.rb
+++ app.rb
@@ -7,12 +7,33 @@ def view(template); erb template.to_sym; end
 before { puts "Parameters: #{params}" }                                     
 
 # enter your Dark Sky API key here
-ForecastIO.api_key = "YOUR-API-KEY"
+ForecastIO.api_key = "ad6d87ce0b95666e8acfc495665df190"
+
+#Enter NewsAPI key
+url = "https://newsapi.org/v2/top-headlines?country=us&apiKey=8f07e516edfb454b84cbbed7239540fb"
 
 get "/" do
+    view "location"
   # show a view that asks for the location
 end
 
 get "/news" do
-  # do everything else
+    @location = params["q"]
+    results = Geocoder.search(params["q"])
+    @lat_long = results.first.coordinates # => [lat, long]
+    @forecast = ForecastIO.forecast(@lat_long[0], @lat_long[1]).to_hash
+    @current_temperature = @forecast["currently"]["temperature"]
+    @conditions = @forecast["currently"]["summary"]
+    @forecast_array = @forecast["daily"]["data"]
+    @days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday"]
+
+    @news = HTTParty.get(url).parsed_response.to_hash
+
+    view "ask"
+    # "In #{@location}, it is currently #{@current_temperature} and #{@conditions}"
+    # "In #{@location}, it is currently #{@current_temperature} and #{@conditions}"
+
+    # forecast_array.each_index do |i|
+    #      "A high temperature of #{forecast_array[i]["temperatureHigh"]} and #{forecast_array["summary"]} for #{days[i]}"
+    # end     
 end
